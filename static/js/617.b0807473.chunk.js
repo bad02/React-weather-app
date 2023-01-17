11b57909944f3d9a803edc2313cbd1009358d773
shivam badoniya <badoniyashivam@gmail.com>
2023-01-18 00:35:43 +0530

    Updates

---
+++ static/js/617.b0807473.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkReact_weather_app=self.webpackChunkReact_weather_app||[]).push([[617],{617:function(t,e,r){r.d(e,{Hi9:function(){return n}});var a=r(983);function n(t){return(0,a.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",stroke:"currentColor"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"}}]})(t)}}}]);
+//# sourceMappingURL=617.b0807473.chunk.js.map
